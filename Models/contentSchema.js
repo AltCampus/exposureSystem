45fb6e0c2097dc880fcdb597f57ec46af81621ef
yjspoder <injosse.joshi@gmail.com>
2019-11-20 13:22:40 +0530

    add content schema

--- Models/contentSchema.js
+++ Models/contentSchema.js
@@ -8,3 +8,26 @@ const Schema = mongoose.Schema;
 
 //Creating content schema
 
+const contentSchema = new Schema({ 
+    type: { 
+        type : "string", 
+        enum: [
+            "resource", 
+            "challenge"
+        ]
+    }, 
+        contentUrl: String, 
+        title: String, 
+        description: String 
+    }, { 
+     timestamps: true 
+    })
+
+//Creating content Model
+
+const Content = mongoose.model("Content" , contentSchema);
+
+
+//Exporting content Model
+
+module.exports = Content;
