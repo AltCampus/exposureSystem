41fce77499daa80487a5077e9efbe08aed31373c
BadBrahmin <tejasshree95@gmail.com>
2019-11-20 16:59:15 +0530

    hello world react webpack done

--- client/index.js
+++ client/index.js
@@ -1,1 +1,5 @@
-console.log("Hey guys and ladies!!");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
