6d8a4a062196d37601d75f4ed8565177d73efbc4
BadBrahmin <tejasshree95@gmail.com>
2019-11-20 16:55:52 +0530

    final webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: "./client/index.html",
+  filename: "index.html",
+  inject: "body"
+});
+
 module.exports = {
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "index_bundle.js"
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+        use: "babel-loader"
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
       }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
