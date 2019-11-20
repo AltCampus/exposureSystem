ecd702973f04afe2fa6226d522ab0f47c4f56e10
sharmaaman4419 <sharmaaman4419@gmail.com>
2019-11-20 15:26:17 +0530

    Add User-Routes

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// Providing The Path
+var usersRouter = require('./routes/UserRoutes');
+var mongoose = require('mongoose');
+// Connecting With DataBase
+mongoose.connect('mongodb://localhost/User', { useNewUrlParser: true }, (err) => {
+	err ? console.log('Not Connected To DB') : console.log('Connected Sucessfully TO DB');
+});
+// Mounting The Express Application
 var app = express();
 
 // view engine setup
@@ -19,23 +23,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
