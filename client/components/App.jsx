e734ff6fceba250ff46f742d4320175489b0c99d
akhil dhiman <dhimanakhil95@gmail.com>
2019-11-21 10:50:32 +0530

    remove conflit from app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
 import Login from './Login/Login'
 import AdminLogin from "./AdminLogin/AdminLogin"
-=======
 import '../style.scss'
->>>>>>> helloWorld#3
 
 class App extends React.Component {
   render() {
