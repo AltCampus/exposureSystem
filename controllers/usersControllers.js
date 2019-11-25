615cf6594a1e1004e93821a875e79cd12a3bb610
sharmaaman4419 <sharmaaman4419@gmail.com>
2019-11-25 10:25:01 +0530

    minor changes

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -2,19 +2,19 @@
 const user = require('../models/userSchema');
 
 // Users Registration
-function creatingUsers(req,res,next){
+function creatingUsers(req, res, next) {
 	user.create(req.body, (err, UserCreated) => {
 		if (err) return next(err);
 		res.status(200).json({ User: UserCreated });
 	});
-};
+}
 
 // All Users Status
-function userStatus(req,res,next){
-    user.find({}, (err, Users) => {
+function userStatus(req, res, next) {
+	user.find({}, (err, Users) => {
 		if (err) console.log(err);
 		res.status(200).json({ users: Users });
 	});
 }
 
-module.exports = {creatingUsers , userStatus};
+module.exports = { creatingUsers, userStatus };
