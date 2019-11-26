05b7d2e6d845763038aa5477fa94ae9a41959429
BadBrahmin <tejasshree95@gmail.com>
2019-11-26 14:20:36 +0530

    rename files

--- client/components/AdminDashboard/AddContent.js
+++ client/components/AdminDashboard/AddContent.js
@@ -1,82 +1,74 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 import Header from "../Header";
 import AdminSidebar from "./AdminSidebar";
 
-class AddContent extends Component{
-    constructor(){
-        super();
-        this.state = {
-            type : "",
-            url : "",
-            title : "",
-            description : ""
-        }
-    }
-    handleChange = (e) => {
-        this.setState({ [e.target.name] : e.target.value});
-    }
-    
-    render(){
-        return(
-            <>
-            <div className="wrapper grid-dashboard">
-                <AdminSidebar />
-                <form className="flex-column flex-center card">
-                   <select 
-                    name="type" 
-                    className="input input-select" 
-                    onClick={this.handleChange}
-                    defaultValue={'DEFAULT'}>
-                        <option 
-                        value="DEFAULT" 
-                        disabled>
-                            Type 
-                        </option>
-                        <option 
-                        className="input" 
-                        value="resource">
-                            Resource
-                        </option>
-                        <option 
-                        className="input" 
-                        value="challenge">
-                            Challenge
-                        </option>
-                    </select>
-                    
-                    <input 
-                    className="input" 
-                    type="text" 
-                    placeholder="Content Url" 
-                    value={this.state.url} 
-                    onChange={this.handleChange}
-                    name="url"></input>
-                    <input 
-                    className="input" 
-                    type="text" 
-                    placeholder="Title"
-                    value={this.state.title}
-                    onChange={this.handleChange}
-                    name="title"></input>
-                    <textarea 
-                    className="input" 
-                    type="text" 
-                    placeholder="Description"
-                    value = {this.state.description}
-                    onChange={this.handleChange}
-                    name="description"></textarea>
-                    <button 
-                    className="button">
-                        Add Content
-                    </button>
-                </form>
+class AddContent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      type: "",
+      url: "",
+      title: "",
+      description: ""
+    };
+  }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-            </div>
-            
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="wrapper grid-dashboard">
+          <AdminSidebar />
+          <form className="flex-column flex-center card">
+            <select
+              name="type"
+              className="input input-select"
+              onClick={this.handleChange}
+              defaultValue={"DEFAULT"}
+            >
+              <option value="DEFAULT" disabled>
+                Type
+              </option>
+              <option className="input" value="resource">
+                Resource
+              </option>
+              <option className="input" value="challenge">
+                Challenge
+              </option>
+            </select>
+
+            <input
+              className="input"
+              type="text"
+              placeholder="Content Url"
+              value={this.state.url}
+              onChange={this.handleChange}
+              name="url"
+            ></input>
+            <input
+              className="input"
+              type="text"
+              placeholder="Title"
+              value={this.state.title}
+              onChange={this.handleChange}
+              name="title"
+            ></input>
+            <textarea
+              className="input"
+              type="text"
+              placeholder="Description"
+              value={this.state.description}
+              onChange={this.handleChange}
+              name="description"
+            ></textarea>
+            <button className="button">Add Content</button>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default AddContent;
--- client/components/AdminDashboard/PendingApprovals.js
+++ client/components/AdminDashboard/PendingApprovals.js
@@ -1,26 +1,26 @@
-import React , {Component} from "react";
+import React, { Component } from "react";
 import AdminSidebar from "./AdminSidebar";
-import Pending from "../Pending";
-class PendingApprovals extends Component{
-    render(){
-        return(
-            <>
-                <div className="wrapper grid-dashboard">
-                    <AdminSidebar />
-                    <div>
-                        <h3 className="flex-center" style={{color : "rgb(59, 57, 57)"}}>Pending Approvals</h3>
-                        <div className="grid-col-3">
-                            <Pending />
-                            <Pending />
-                            <Pending />
-                        </div>
-                    </div>
-                    
-                </div>
-            </>
-        )
-    }
+import PendingCard from "../PendingCard";
+class PendingApprovals extends Component {
+  render() {
+    return (
+      <>
+        <div className="wrapper grid-dashboard">
+          <AdminSidebar />
+          <div>
+            <h3 className="flex-center" style={{ color: "rgb(59, 57, 57)" }}>
+              Pending Approvals
+            </h3>
+            <div className="grid-col-3">
+              <PendingCard />
+              <PendingCard />
+              <PendingCard />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default PendingApprovals;
