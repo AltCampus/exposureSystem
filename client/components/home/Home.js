889c3a0acd1845e16569a1114b0b098f3db60b0e
yjspoder <injosse.joshi@gmail.com>
2019-11-27 14:04:34 +0530

    add changes to student card

--- client/components/home/Home.js
+++ client/components/home/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Header from "../header/Header";
 
+
+
 class Home extends React.Component {
   render() {
     return (
       // <Header />
-      <></>
+      <div className="wrapper">
+        <div className="text-center home-description">
+          <p>An exciting new initiative to expose</p>
+          <h4>
+            Humans of 
+            <img className="alt-image" src="https://altcampus.io/images/logo.png"></img>
+          </h4>
+          <p>to various ideas and opinions other than just programming</p> 
+        </div>
+      </div>
     )
   }
 }
