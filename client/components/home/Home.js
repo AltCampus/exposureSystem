945c79a96f3e3f6bdda6113d95f0238d2c989ffe
yjspoder <injosse.joshi@gmail.com>
2019-11-27 14:20:06 +0530

    add landing page

--- client/components/home/Home.js
+++ client/components/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../header/Header";
+import { NavLink} from "react-router-dom"
 
 
 
@@ -8,13 +9,14 @@ class Home extends React.Component {
     return (
       // <Header />
       <div className="wrapper">
-        <div className="text-center home-description">
+        <div className="text-center home-description flex-center">
           <p>An exciting new initiative to expose</p>
           <h4>
             Humans of 
             <img className="alt-image" src="https://altcampus.io/images/logo.png"></img>
           </h4>
           <p>to various ideas and opinions other than just programming</p> 
+          <NavLink className="button" to="/signup">Register</NavLink>
         </div>
       </div>
     )
