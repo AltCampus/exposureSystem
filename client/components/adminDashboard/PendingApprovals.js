c1e2e55cdc62c4ff8ae09ac736c9459b5895fdac
akhil dhiman <dhimanakhil95@gmail.com>
2019-11-28 14:47:41 +0530

    display pending student details in pendng approvals

--- client/components/adminDashboard/PendingApprovals.js
+++ client/components/adminDashboard/PendingApprovals.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import AdminSidebar from "./AdminSidebar";
-import Pending from "./PendingCard";
+import PendingCard from "./PendingCard";
 
 class PendingApprovals extends Component {
     constructor() {
@@ -9,7 +9,6 @@ class PendingApprovals extends Component {
             pendingStudentList: null
         }
     }
-
 
     componentDidMount() {
         fetch("/admin/pending", {
@@ -23,7 +22,8 @@ class PendingApprovals extends Component {
     }
 
     render() {
-        console.log(this.state)
+        const pendingStudentList = (this.state.pendingStudentList && this.state.pendingStudentList.users)
+        console.log(pendingStudentList)
         return (
             <>
                 <div className="wrapper grid-dashboard">
@@ -31,9 +31,11 @@ class PendingApprovals extends Component {
                     <div>
                         <h3 className="flex-center" style={{ color: "rgb(59, 57, 57)" }}>Pending Approvals</h3>
                         <div className="grid-col-3">
-                            <Pending />
-                            <Pending />
-                            <Pending />
+                            {
+                                pendingStudentList && pendingStudentList.map(pendingStudent => {
+                                    return <PendingCard pendingStudentData={pendingStudent} />
+                                })
+                            }
                         </div>
                     </div>
 
