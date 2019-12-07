3bcfe8146520c0e6905f805cd9d3b672f7fcc50c
akhil dhiman <dhimanakhil95@gmail.com>
2019-12-07 21:56:50 +0530

    add status messages for addcontent. editcontent, submitcontent, register, login, approved and rejected

---
+++ client/components/statusMessages/ContentSubmissionMessage.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function ContentSubmissionMessage() {
+    return (
+        <div>
+            <h4 style={{color: "green"}}>This content has been successfully submitted</h4>
+        </div>
+    )
+}
+
+
+
+export default ContentSubmissionMessage
