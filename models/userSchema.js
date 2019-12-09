0629801ddedbf887019cca1062b0d2a436b59c0e
sharmaaman4419 <sharmaaman4419@gmail.com>
2019-12-09 13:36:08 +0530

    implement validations

--- models/userSchema.js
+++ models/userSchema.js
@@ -13,7 +13,8 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-      required: true
+      required: true,
+      unique: true
     },
     email: {
       type: String,
