5d338077efac298c90d04b576bc10978410824b4
BadBrahmin <tejasshree95@gmail.com>
2019-12-10 10:05:58 +0530

    add some mailer

---
+++ utils/nodeMailer.js
@@ -0,0 +1,6 @@
+// var sender = 'smtps://prashant%40altcampus.io';
+
+// var password = 'yourEmailPassword';
+
+// var nodeMailer = require('nodemailer');
+// var EmailTemplate = require('email-templates').EmailTemplate;
