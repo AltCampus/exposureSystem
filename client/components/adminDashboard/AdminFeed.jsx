1f968637daaed0c0cc964e1085205c0d8950eb0f
BadBrahmin <tejasshree95@gmail.com>
2019-12-14 10:34:02 +0530

    add static submission card in dashboard

--- client/components/adminDashboard/AdminFeed.jsx
+++ client/components/adminDashboard/AdminFeed.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import StudentSubmissionCard from '../students/studentDashboard/StudentSubmissionCard';
 import AdminSidebar from './AdminSidebar';
+// import Header from '../header/Header';
 import { connect } from 'react-redux';
 import { fetchSubmissionList } from '../../redux/actions/submissonAction';
 
@@ -17,15 +18,19 @@ class AdminFeed extends Component {
   render() {
     return (
       <>
+        {/* <Header /> */}
         <div className="wrapper grid-dashboard">
           <div>
             <AdminSidebar />
           </div>
           <div className="grid-col-1">
-            {this.state.submissionList &&
+            {/* {this.state.submissionList &&
               this.state.submissionList.map(submission => {
                 <StudentSubmissionCard submission={submission} />;
-              })}
+              })} */}
+            <StudentSubmissionCard />
+            <StudentSubmissionCard />
+            <StudentSubmissionCard />
           </div>
         </div>
       </>
--- client/components/students/studentDashboard/StudentSubmissionCard.jsx
+++ client/components/students/studentDashboard/StudentSubmissionCard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 // TODO : MAKE IT AS WHEN CLICKED ON THE CARD IT OPENS THE ORIGINAL ARTICLE + SUBMISSION (GET)
 class StudentSubmissionCard extends Component {
   constructor(props) {
@@ -7,21 +7,23 @@ class StudentSubmissionCard extends Component {
   }
 
   render() {
-    const { title , contentSummary } = this.props.submission;
+    // const { title, contentSummary } = this.props.submission;
     return (
       // <NavLink>
-        <div className="wrapper flex-column">
-          <div className="flex-column">
-            <h5 className="card-heading flex-center">Title : {title}</h5>
-            <h5 className="card-details">Submitted Summary:</h5>
-            <p>{contentSummary}</p>
-            <div className="card-footer">Created at:</div>
-          </div>
+      <div className="wrapper flex-column">
+        <div className="flex-column">
+          <h5 className="card-heading flex-center">
+            Title : This is the title
+          </h5>
+          <h5 className="card-details">Submitted Summary: </h5>
+          <p>This is the summary of the submission</p>
+          <div className="card-footer">Created at: 01/01/2020</div>
         </div>
+      </div>
       // </NavLink>
     );
   }
 }
 
-const mapStateToProps = (state) => state;
+const mapStateToProps = state => state;
 export default StudentSubmissionCard;
