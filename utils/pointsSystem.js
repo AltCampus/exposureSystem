81b79442c12e4969c6ae74b815d2aed207229424
akhil dhiman <dhimanakhil95@gmail.com>
2019-12-17 00:55:22 +0530

    add point system function

--- utils/pointsSystem.js
+++ utils/pointsSystem.js
@@ -87,3 +87,79 @@
 // updateGroupOptout(group);
 
 // module.exports = { updateIndividualPoints };
+
+
+
+
+
+const updatePoints = (users, type) => {
+    // define a callback that doesnt change our data
+    // so even if you remove the default error case below, the function wouldnt crash
+    // when you add a new type
+    let callback = user => user;
+  
+    // in each known case, we overwrite above callback with a function specific for this case
+    switch (type) {
+      case "individual":
+        callback = user => ({
+          ...user,
+          // if the user has submitted, add 0.5 points, else subtract 1
+          // always from the previous points
+          points: user.points + user.hasSubmitted ? 0.5 : -1
+        });
+        break;
+      case "pair":
+        callback = user => ({
+          ...user,
+          // if the user has submitted, add 1 point, else subtract 1
+          // always from the previous points
+          points: user.points + user.hasSubmitted ? 1 : -1
+        });
+        break;
+      case "group":
+        // if one of the group members submitted, point change will differ
+        // findIndex returns the index/position of a dataset within an array
+        // if it _doesnt_ find anything, it will return -1
+        // so index > -1 is only true if it found a dataset
+        const atLeastOneUserSubmitted =
+          users.findIndex(user => user.hasSubmitted === true) > -1;
+  
+        if (atLeastOneUserSubmitted) {
+          callback = user => ({
+            ...user,
+            // if the user has submitted, add 0 point, else subtract 1
+            // always from the previous points
+            points: user.points + user.hasSubmitted ? 0 : -1
+          });
+          break;
+        }
+  
+        // always subtract 2 from the previous points since nobody submitted
+        callback = user => ({ ...user, points: user.points - 2 });
+        break;
+      default:
+        throw new Error("missing case");
+    }
+  
+    // callback will be executed for every element within users
+    return users.map(callback);
+  };
+  
+  class User {
+    constructor(name, points=0, hasSubmitted = false) {
+      this.name = name;
+      this.points = points;
+      this.hasSubmitted = hasSubmitted;
+    }
+  }
+  
+  const users = [
+    new User("Rick", 0, true),
+    new User("Jane", 0, false),
+    new User("Jamie", 0, true)
+  ];
+  
+  console.log("individual", updatePoints([users[0]], "individual"));
+  console.log("pair", updatePoints([users[0], users[1]], "pair"));
+  console.log("group", updatePoints(users, "group"));
+  
