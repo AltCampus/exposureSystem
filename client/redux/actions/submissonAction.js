5e410d0fbf7c91361ae2a3c71fb8bd8861f426b7
BadBrahmin <tejasshree95@gmail.com>
2019-12-17 12:50:25 +0530

    add submission actions

--- client/redux/actions/submissonAction.js
+++ client/redux/actions/submissonAction.js
@@ -1,3 +1,26 @@
+// const fetchDeliveryData
+
+const createSubmission = (submissionData, cb) => dispatch => {
+  dispatch({
+    type: 'CREATE_NEW_SUBMISSION_START',
+  });
+  fetch('http://localhost:3000/api/v1/submission/new', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: submissionData,
+  })
+    .then(res => res.json())
+    .then(submission => {
+      dispatch({
+        type: 'NEW_SUBMISSION_CREATED_SUCCESSFULLY',
+        data: submissionData,
+      }),
+        cb();
+    });
+};
+
 const fetchSubmissionList = () => dispatch => {
   dispatch({
     type: 'FETCHING_SUBMISSION_LIST_START',
